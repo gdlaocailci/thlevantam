1ae87e34759e0fd0bf5798f69e213dc1760fc5fa
Hoàng Ngọc Lâm <hoangngoclamlc@gmail.com>
2026-09-15 08:46:54 +0700

    Update KetNoi.js

--- ThoiKhoaBieu_LVT/KetNoi.js
+++ ThoiKhoaBieu_LVT/KetNoi.js
@@ -1,7 +1,7 @@
 const CAU_HINH_FRONTEND = {
     URL_API_MAY_CHU: 'https://script.google.com/macros/s/AKfycbyvZXo2zJhnK60uopeMxwTaW8FS0YMiMl5606EAuYqgYB78-StMwuaLyRCJEyycQRNx/exec',
     TEN_DU_AN: 'THỜI KHÓA BIỂU & SỔ ĐẦU BÀI THÔNG MINH',
-    
+    MA_DU_AN: 'DU_AN_LEVANTAM',
     // Cấu hình chữ ký Footer hiển thị 2 dòng
     TIEU_DE_TAC_GIA: 'Thiết kế và phát triển',
     TAC_GIA_THIET_KE: 'Hoàng Ngọc Lâm',
