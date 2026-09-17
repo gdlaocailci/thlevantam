3dc4d87ccdc28de7507b4e267d3229803536b45b
Hoàng Ngọc Lâm <hoangngoclamlc@gmail.com>
2026-09-17 17:54:35 +0700

    Add files via upload

---
+++ qlhslvt/ketnoi.js
@@ -0,0 +1,7 @@
+// CẤU HÌNH HỆ THỐNG GIAO TIẾP VÀ HIỂN THỊ (CLIENT)
+// ===========================================================================
+const HKS_CAU_HINH_WEBAPP_STR = "https://script.google.com/macros/s/AKfycbzfIjwSriEXA-aLLxk8reQsF0b8sjpP2P1KyGNYiMsOdr-d_q4RR5gP8dVIzbiFHmnz/exec"; 
+const HKS_CAU_HINH_CLIENT_ID_STR = "1097384743947-1jdc5rhhmbu0s9jp5vgt814g4f4id7lu.apps.googleusercontent.com";
+const HKS_CAU_HINH_LOGO_STR = "https://i.postimg.cc/xjzpvXCm/Logo-HL.png";
+const HKS_CAU_HINH_TEN_DINH_DANH_STR = "Trường TH&THCS Tả Phời";
+const HKS_CAU_HINH_TAC_GIA_STR = "Hoàng Ngọc Lâm";
