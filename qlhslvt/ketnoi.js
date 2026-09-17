4967d25a0f4d507355e6546892967f7de4608fee
Hoàng Ngọc Lâm <hoangngoclamlc@gmail.com>
2026-09-17 18:06:10 +0700

    Update ketnoi.js

--- qlhslvt/ketnoi.js
+++ qlhslvt/ketnoi.js
@@ -3,5 +3,5 @@
 const HKS_CAU_HINH_WEBAPP_STR = "https://script.google.com/macros/s/AKfycbxymCtDe1L3oBYEDJbs6qk84GAzGwvGjZTbBCdRRMVy_QAnFSWwesJ0OcRzW0YODz9w3w/exec"; 
 const HKS_CAU_HINH_CLIENT_ID_STR = "1097384743947-1jdc5rhhmbu0s9jp5vgt814g4f4id7lu.apps.googleusercontent.com";
 const HKS_CAU_HINH_LOGO_STR = "https://i.postimg.cc/xjzpvXCm/Logo-HL.png";
-const HKS_CAU_HINH_TEN_DINH_DANH_STR = "Trường TH&THCS Tả Phời";
+const HKS_CAU_HINH_TEN_DINH_DANH_STR = "Trường Tiểu học Lê Văn Tám";
 const HKS_CAU_HINH_TAC_GIA_STR = "Hoàng Ngọc Lâm";
